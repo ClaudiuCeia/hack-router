c818fca042117f3811e6a896c22489a13f7ed6f3
Fred Emmott <github@fredemmott.co.uk>
2015-12-21 00:46:16 +0000

    Try to make Hack docks site re-usable
    First step. Just putting it up so I can pull it into a few projects easily to
    see how well it works.
    
    refs hhvm/user-documentation#206

---
+++ src/NotFoundException.php
@@ -0,0 +1,23 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) 2015, Fred Emmott
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+namespace FredEmmott\HackRouter;
+
+final class NotFoundException extends RoutingException {
+  public function __construct(string $method, string $path) {
+    parent::__construct(
+      "Not Found: ".$path,
+      $method,
+      $path,
+    );
+  }
+}
