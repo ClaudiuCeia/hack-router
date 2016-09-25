d6cbe2f131215126bd8906ae131ec87af060c60f
Fred Emmott <fred@fredemmott.co.uk>
2016-09-25 09:04:19 -0700

    Split up UriParameters
     - UriParametersBase: core functionality
     - UriParametersGetters: getString and so on
     - UriParameters: concrete implementation
    
    refs #3

---
+++ src/uripatterns/UriParametersBase.php
@@ -0,0 +1,49 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2015, Fred Emmott
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
+namespace FredEmmott\HackRouter;
+
+abstract class UriParametersBase {
+  private ImmMap<string, UriPatternParameter> $specs;
+
+  public function __construct(
+    Traversable<UriPatternParameter> $spec_vector,
+    protected ImmMap<string, string> $values,
+  ) {
+    $specs = Map { };
+    foreach ($spec_vector as $spec) {
+      $specs[$spec->getName()] = $spec;
+    }
+    $this->specs = $specs->immutable();
+  }
+
+  final protected function getSpec<T as UriPatternParameter>(
+    classname<T> $class,
+    string $name,
+  ): T {
+    $spec = $this->specs->at($name);
+    invariant(
+      $spec instanceof $class,
+      'Expected %s to be a %s, got %s',
+      $name,
+      $class,
+      get_class($spec),
+    );
+    return $spec;
+  }
+
+  final protected function getSimpleTyped<T>(
+    classname<UriPatternTypedParameter<T>> $class,
+    string $name,
+  ): T {
+    $spec = $this->getSpec($class, $name);
+    $value = $this->values->at($name);
+    return $spec->assert($value);
+  }
+}
